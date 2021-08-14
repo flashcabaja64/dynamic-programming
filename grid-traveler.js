3f58fb2919cf955af0a345477257aa54c941a3bc
Hubert Yang <offspring7890@yahoo.com>
2021-08-13 23:14:34 -0700

    grid-traveler memo

--- grid-traveler.js
+++ grid-traveler.js
@@ -10,3 +10,17 @@ console.log(gridTraveler(2, 3))
 console.log(gridTraveler(3, 2))
 console.log(gridTraveler(3, 3))
 //console.log(gridTraveler(18, 18))
+
+function gridMemo(m, n, memo = {}) {
+  const key = `${m} , ${n}`;
+
+  if(key in memo) return memo[key];
+  if(m === 1 && n === 1) return 1;
+  if(m === 0 || n === 0) return 0;
+
+  memo[key] = gridMemo(m - 1, n, memo) + gridMemo(m, n - 1, memo);
+
+  return memo[key]
+}
+
+//console.log(gridMemo(18, 18))
